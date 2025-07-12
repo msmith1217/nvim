4a65d3e8259c5002c67ccad015922af591aac27f
msmith1217 <macsmith12@proton.me>
2025-07-12 16:23:04 -0400

    lsp updates

--- init.lua
+++ init.lua
@@ -1,1 +1,15 @@
 require("macsnvim")
+vim.lsp.enable('luals')
+vim.lsp.enable('zls')
+vim.lsp.enable('pyright')
+vim.lsp.enable('html')
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+  severity_sort = false,
+  float = true,
+})
+
+
